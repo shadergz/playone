e5b3e13cf9174f31a1f8bcf6892a61ace104a3d9
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-26 19:04:27 -0300

    [0.0.1] CpuMipsR3000A, (fetch, decode and execute) was added

---
+++ src/hard_common.rs
@@ -0,0 +1,4 @@
+
+pub type REGDword = u32;
+// pub type REGWord = u16;
+
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod hard_common;
+mod dram_edo;
+mod mips_cortex;
+
+use crate::mips_cortex::CpuMipsR3000A;
+
+extern crate glium;
+
+struct PsxConsole {
+    central_unit : CpuMipsR3000A
+}
+
+impl PsxConsole {
+    pub fn new() -> PsxConsole {
+        PsxConsole {
+            central_unit: CpuMipsR3000A::new()
+        }
+    }
+
+    fn power_on(&mut self) {
+        // Performing a reset signal inside all playstation components/hardware(s)
+        PsxConsole::console_hardware_reset(self);
+
+    }
+    fn console_hardware_reset(&mut self) {
+        let console_cpu = &mut self.central_unit;
+        console_cpu.cpu_power_reset();
+    }
+}
+
+pub fn main() {
+    let mut playone_console = PsxConsole::new();
+    playone_console.power_on();
+}
